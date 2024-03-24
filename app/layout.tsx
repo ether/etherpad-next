781653beeb8eadd2f7eaae8fed5a50f4111f70b3
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-03-24 21:37:25 +0100

    Feat(pwa): setup basic (#65)
    * feat(PWA):introduce
    
    * feat(pwa): setup manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import getSetting from '@/lib/settings';
 import type { FC, PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
 import '@/styles/globals.css';
+
+const settings = await getSetting();
+
+const metadata: Metadata = {
+  manifest: '/manifest.webmanifest',
+  description: settings.title,
+};
 
 const Layout: FC<PropsWithChildren> = ({ children }) => (
   <html lang="en">
@@ -7,4 +16,5 @@ const Layout: FC<PropsWithChildren> = ({ children }) => (
   </html>
 );
 
+export { metadata };
 export default Layout;
